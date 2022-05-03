ed0f67789c790960f79e0d52d8e7cd2dbdd4a0c1
aSharKaw <shoumaarakawa06@gmail.com>
2022-05-03 21:49:00 +0900

    4-1〜4-11まで作成

--- JavaTraining4.java
+++ JavaTraining4.java
@@ -6,13 +6,17 @@ public class JavaTraining4 {
 
     public static void main(String[] args) throws IOException
     {
-        //11までで一回チェック
-
-        //Four_One();
-        //Four_Two();
-        //Four_Three();
-        //Four_Four();
-        //Four_Five();
+        Four_One();
+        Four_Two();
+        Four_Three();
+        Four_Four();
+        Four_Five();
+        Four_Six();
+        Four_Seven();
+        Four_Eight();
+        Four_Nine();
+        Four_Ten();
+        Four_Eleven();
     }
 
     //4-1
@@ -77,5 +81,144 @@ public class JavaTraining4 {
         }
 
         System.out.println("平均値は" + (num / 10));
+    }
+
+    //4-6
+    public static void Four_Six() throws IOException
+    {
+        System.out.println("\n4-6");
+        int win_count, lose_count;
+
+        win_count = 0;
+        lose_count = 0;
+
+        for(int i = 0; i < 10; i++)
+        {
+            if(Integer.parseInt(br.readLine()) == 0)
+            {
+                lose_count++;
+            }
+            else
+            {
+                win_count++;
+            }
+        }
+        System.out.println("勝ち:" + win_count + ", 負け:" + lose_count);
+    }
+
+    //4-7
+    public static void Four_Seven() throws IOException
+    {
+        System.out.println("\n4-7");
+        int giants_score, tigers_score;
+
+        giants_score = 0;
+        tigers_score = 0;
+
+        for(int lound = 1; lound <= 9; lound++)
+        {
+            System.out.print(lound + "回表、巨人の得点は？");
+            giants_score += Integer.parseInt(br.readLine());
+            
+            System.out.print(lound + "回裏、阪神の得点は？");
+            tigers_score += Integer.parseInt(br.readLine());
+            
+        }
+        System.out.println("\n巨人：" + giants_score + "点, 阪神：" + tigers_score + "点");
+
+        if(giants_score == tigers_score)
+        {
+            System.out.println("引き分け");
+        }
+        else if(giants_score > tigers_score)
+        {
+            System.out.println("巨人の勝ち");
+        }
+        else
+        {
+            System.out.println("阪神の勝ち");
+        }
+    }
+
+    //4-8
+    public static void Four_Eight() throws IOException
+    {
+        System.out.println("\n4-8");
+        int max_num = 0;
+
+        for(int i = 0; i < 10; i++)
+        {
+            int num = Integer.parseInt(br.readLine());
+            if(num > max_num || i == 0)
+            {
+                max_num = num;
+            }
+        }
+        System.out.println("最大値は" + max_num);
+    }
+
+    //4-9
+    public static void Four_Nine() throws IOException
+    {
+        System.out.println("\n4-9");
+        int max_num, min_num;
+        max_num = 0;
+        min_num = 0;
+
+        for(int i = 0; i < 10; i++)
+        {
+            int num = Integer.parseInt(br.readLine());
+            if(i == 0)
+            {
+                max_num = num;
+                min_num = num;
+            }
+            else if(num > max_num)
+            {
+                max_num = num;
+            }
+            else if(num < min_num)
+            {
+                min_num = num;
+            }
+        }
+        System.out.println("最大値は" + max_num + "、最小値は" + min_num);
+    }
+
+    //4-10
+    public static void Four_Ten() throws IOException
+    {
+        System.out.println("\n4-10");
+
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < num; i++)
+        {
+            System.out.print("*");
+        }
+        //行間調整のため改行
+        System.out.println();
+    }
+
+    //4-11
+    public static void Four_Eleven() throws IOException
+    {
+        System.out.println("\n4-11");
+
+        int num = Integer.parseInt(br.readLine());
+        int output_num = 0;
+
+        for(int i = 0; i < num; i++)
+        {
+            System.out.print(output_num);
+            output_num++;
+            
+            if(output_num == 10)
+            {
+                output_num = 0;
+            }
+        }
+        //行間調整のため改行
+        System.out.println();
     }
 }
